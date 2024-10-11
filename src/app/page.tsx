a75a5d2c879be41b832d2873f7f5c03676858e3a
Alex <162977380+theantiheroSE@users.noreply.github.com>
2024-10-11 21:27:00 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic'
 
-const AntiHero = dynamic(() => import('@/components/AntiHero'), { ssr: false })
+const AntiHero = dynamic(() => import('../components/AntiHero'), { ssr: false })
 
 export default function Home() {
   return (
